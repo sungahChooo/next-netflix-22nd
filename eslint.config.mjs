98a97a6d05fafa5ce8aade296a6a608da8385e0f
sungahChooo <170587664+sungahChooo@users.noreply.github.com>
2025-10-31 21:40:56 +0900

    chore: eslint, prettier 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,32 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+// eslint.config.mjs
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
+  files: ['**/*.{ts,tsx,js,jsx}'],
+  ignores: ['node_modules', 'dist', '.next'],
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.node,
+    },
+  },
+  plugins: {
+    react,
+    prettier,
+  },
+  rules: {
+    ...react.configs.recommended.rules,
+    'react/react-in-jsx-scope': 'off', // Next.js에서는 불필요
+    'prettier/prettier': 'error', // prettier 규칙을 ESLint에 통합
+    '@typescript-eslint/no-unused-vars': ['warn'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+});
