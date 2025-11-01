b30dd90d8ea5a993cd9b7d71a5d29a1579815316
baekseungsun <baekjimmy@yonsei.ac.kr>
2025-11-01 10:58:41 +0900

    refactor: 마진 조정

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -14,27 +14,27 @@ import ThrillerMysteries from "@/components/TvThrillerMysteries";
 import UsTvShow from "@/components/USTvShow";
 import WatchItAgain from "@/components/WatchItAgain";
 export default async function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-black ">
-      <div className="w-[375px] bg-transparent relative flex flex-col overflow-hidden pb-4">
-        <div className="h-16 bg-black text-white flex items-center justify-center">
-          <HomeHeader />
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-black">
+            <div className="w-[375px] bg-transparent relative flex flex-col overflow-hidden mb-20">
+                <div className="h-16 bg-black text-white flex items-center justify-center">
+                    <HomeHeader />
+                </div>
+                <Banner />
+                <Previews />
+                <ContinueWataching />
+                <Popular />
+                <TrendingNow />
+                <Top10 />
+                <MyList />
+                <AfricanMovies />
+                <Nollywood />
+                <NetflixOriginals />
+                <WatchItAgain />
+                <NewReleases />
+                <ThrillerMysteries />
+                <UsTvShow />
+            </div>
         </div>
-        <Banner />
-        <Previews />
-        <ContinueWataching />
-        <Popular />
-        <TrendingNow />
-        <Top10 />
-        <MyList />
-        <AfricanMovies />
-        <Nollywood />
-        <NetflixOriginals />
-        <WatchItAgain />
-        <NewReleases />
-        <ThrillerMysteries />
-        <UsTvShow />
-      </div>
-    </div>
-  );
+    );
 }
