b9d8a4fcfc85566c2f16a18f80033d0d93316077
sungahChooo <170587664+sungahChooo@users.noreply.github.com>
2025-11-01 15:35:52 +0900

    fix: 헤더와 navbar UI 초과 해결

--- components/home/HomeHeader.tsx
+++ components/home/HomeHeader.tsx
@@ -3,9 +3,9 @@
 import logo from "@/public/icons/logo.svg";
 import Image from "next/image";
 
-export default function Header() {
+export default function HomeHeader() {
   return (
-    <header className="fixed pt-[24px] top-0 w-[375px] left-1/2 -translate-x-1/2 z-50 bg-gradient-to-b from-black/80 via-black/50 to-transparent backdrop-blur-sm flex items-center">
+    <header className="fixed pt-6 top-0 w-93 left-1/2 -translate-x-1/2 z-50 bg-gradient-to-b from-black/80 via-black/50 to-transparent backdrop-blur-sm flex items-center">
       <div className="text-red-600 font-extrabold text-2xl select-none">
         <Image src={logo} className="w-[57px] h-[57px]" alt={logo} />
       </div>
